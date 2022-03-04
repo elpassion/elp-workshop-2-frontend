8b4bcda03659e539430055e560d884b55e7237b6
Lukasz Konatowski <konat1993@gmail.com>
2022-03-04 22:38:14 +0100

    testing if request passes after clicking button

--- cypress/integration/weather.spec.ts
+++ cypress/integration/weather.spec.ts
@@ -1,20 +1,23 @@
-/* eslint-disable no-undef */
+/* eslint-disable testing-library/await-async-utils */
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
 describe("example", () => {
-  it("shows data", () => {
+  beforeEach(() => {
     websiteIsOpened()
+  })
 
-
+  it("shows data", () => {
+    cy.intercept('GET', '/data/2.5/weather?q=Gdynia,pl&appid=d45fdad17e3b225c88a4be40c4a6ee2b&units=metric').as('weather-data')
     testTabByValue('.tab-1', 'Open Weather Map API')
 
     testInputByLabel('label[for="latitude"]', 'Latitude', '58.93')
     testInputByLabel('label[for="longitude"]', 'Longitude', '13.58')
 
-    testConfirmationButton('.confirm-btn', 'Find Weather Details')
+    testConfirmationButtonAPIRequest('.confirm-btn', 'Find Weather Details')
 
     // testTabByValue('.tab-2', 'Rapid Weather API')
+
   })
 })
 
@@ -36,9 +39,9 @@ const testInputByLabel = (selectorName: string, containName: string, coordValue:
   //Check input from current label
   cy.get(`#${containName.toLowerCase()}`).should("have.value", coordValue)
 }
-const testConfirmationButton = (selectorName: string, containName: string) => {
+const testConfirmationButtonAPIRequest = async (selectorName: string, containName: string) => {
   const btn = cy.get(selectorName)
   btn.contains(containName)
   btn.click()
-  cy.focused().contains(containName)
+  cy.wait('@weather-data').its('response.statusCode').should('eq', 200)
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import React from "react"
+import { useAsyncFn } from "react-use"
+
+type SynthEvent = React.SyntheticEvent<Element, Event>
+
+const getURL = (reqValue: string): string => {
+  let URL = ''
+  if (reqValue === 'Open Weather Map API') {
+    URL = 'https://api.openweathermap.org/data/2.5/weather?q=Gdynia,pl&appid=d45fdad17e3b225c88a4be40c4a6ee2b&units=metric'
+  } else {
+    URL = 'anotherHTTPRequest'
+  }
+  return URL
+}
 
 const App: React.FC = () => {
+  const [tabValue, setTabValue] = React.useState('Open Weather Map API')
+  const [{ value, loading, error }, fetchWeather] = useAsyncFn(async () => {
+    const response = await fetch(getURL(tabValue))
+    const result = await response.json()
+    return result
+  }, [tabValue])
+
+  const handleClick = (event: SynthEvent) => {
+    setTabValue((event.target as HTMLParagraphElement).innerText)
+  }
+  console.log("tabValue", tabValue, value)
   return (
     <>
       <div>
-        <p className="tab-1">Open Weather Map API</p>
-        <p className="tab-2">Rapid Weather API</p>
+        <p className="tab-1" onClick={handleClick}>Open Weather Map API</p>
+        <p className="tab-2" onClick={handleClick}>Rapid Weather API</p>
       </div>
       <label htmlFor="latitude">Latitude</label>
       <input id="latitude" type="number" />
       <label htmlFor="longitude">Longitude</label>
       <input id="longitude" type="number" />
-      <button className="confirm-btn">Find Weather Details</button>
+      <button className="confirm-btn" onClick={fetchWeather}>Find Weather Details</button>
     </>
   )
 }
 
 export default App
+
+
